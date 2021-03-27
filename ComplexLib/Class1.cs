eb59fc23a4e89eb2f6860db4ad3367daacf9de53
Shukri Adams <shukri.adams@gmail.com>
2021-03-27 21:03:35 +0100

    moving in

---
+++ ComplexLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using SimpleLib;
+
+namespace ComplexLib
+{
+    public class Complex
+    {
+        public string DoComplex()
+        {
+            return Simple.DoSomething() + " complex 1";
+        }
+        
+    }
+}
